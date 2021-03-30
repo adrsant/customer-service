2e1af7ebfa7f2da0ab83aa4fb4b8cceb19f101f0
adriano.santos <adriano.santos@contaazul.com>
2021-03-30 08:43:56 -0300

    chore: improve api documentation

--- src/main/java/com/luizalabs/infrastructure/configuration/SpringFoxConfiguration.java
+++ src/main/java/com/luizalabs/infrastructure/configuration/SpringFoxConfiguration.java
@@ -1,13 +1,23 @@
 package com.luizalabs.infrastructure.configuration;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -21,6 +31,44 @@ public class SpringFoxConfiguration {
         .select()
         .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
         .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiInfo())
+        .securitySchemes(List.of(apiKey()))
+        .securityContexts(Arrays.asList(securityContext()));
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("Customer API")
+        .license("Apache License Version 2.0")
+        .version("0.0.1")
         .build();
+  }
+
+  @Bean
+  public SecurityConfiguration security() {
+    return SecurityConfigurationBuilder.builder()
+        .scopeSeparator(",")
+        .additionalQueryStringParams(null)
+        .useBasicAuthenticationWithAccessCodeGrant(false)
+        .build();
+  }
+
+  private ApiKey apiKey() {
+    return new ApiKey("apiKey", "Authorization", "header");
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .forPaths(PathSelectors.any())
+        .build();
+  }
+
+  private List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return Arrays.asList(new SecurityReference("apiKey", authorizationScopes));
   }
 }
